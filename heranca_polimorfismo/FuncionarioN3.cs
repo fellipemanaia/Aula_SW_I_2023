debf2d02e89732dddddfdd5d4998ed4d61884c13
Fellipe Manaia <100931530+fellipemanaia@users.noreply.github.com>
2023-05-02 10:51:36 -0300

    Herança e polimorfismo C# VSCode 02/05

---
+++ heranca_polimorfismo/FuncionarioN3.cs
@@ -0,0 +1,9 @@
+﻿namespace heranca_polimorfismo;
+class FuncionarioN3:Funcionario
+{
+   public string? Cargo {get;set;}
+    public override double Participacao(){
+    return base.Participacao() * 0.03;
+
+   }
+}
